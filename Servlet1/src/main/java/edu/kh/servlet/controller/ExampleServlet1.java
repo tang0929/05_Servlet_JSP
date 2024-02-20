a142acaa9e38e900b11f5d4f8d9896257b2b5ded
NTH <tang0929@naver.com>
2024-02-20 16:09:01 +0900

    20240220

---
+++ Servlet1/src/main/java/edu/kh/servlet/controller/ExampleServlet1.java
@@ -0,0 +1,96 @@
+package edu.kh.servlet.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet; // Tomcat이 제공하는 클래스
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ExampleServlet1 extends HttpServlet{
+
+	
+	// GET 방식 요청(Method = "get"인 form 태그 제출) 처리하는 메서드
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		/* 매개변수 req, resp 
+		 * 
+		 * HttpServletRequest
+		 *  - 클라이언트 요청 시 자동 생성되는 객체
+		 *  - 요청 시 전달된 데이터, 요청한 클라이언트 정보
+		 *  - 요청을 유연하게 처리하기 위한 객체 등을 제공
+		 *  
+		 * HttpServletResponse
+		 *  - 클라이언트 요청 시 자동 생성되는 객체
+		 *  - 서버가 클라이언트에게 응답할 수 있는 방법을 제공(서버 -> 클라이언트 출력 스트림(PrintWriter))
+		 *  
+		 */
+		
+
+		// 요청 시 같이 제출된 이름, 나이 얻어오기 
+		// -> 제출된 이름, 나이 == 전달 인자(Parameter) == 전달 받은 값
+		
+		// String HttpServletRequest.getParameter("name속성값");
+		
+		// <input name = "inputName">에 작성된 제출된 값 얻어오기
+		String inputName = req.getParameter("inputName");
+		
+		// <input age = "inputAge">에 작성된 제출된 값 얻어오기
+		String inputAge = req.getParameter("inputAge");
+		
+		
+		System.out.println("[서버] /ex 요청을 받음");
+		System.out.println("전달 받은 inputName : "+ inputName);
+		System.out.println("전달 받은 inputAge : "+ inputAge);
+		
+		
+		
+		
+		//---------------------------------------------------------
+		/* 응답 처리
+		 * 
+		 * 서버(현재 프로젝트) -> 클라이언트(브라우저)
+		 * HTML 코드를 출력 -> 출력받은 HTML 코드를 해석해서 화면 구성
+		 * 
+		 * 1) 응답하는 문서(데이터)의 형식과 문자 인코딩 지정
+		 * 
+		 * 2) 출력용 스트림 얻어오기
+		 * 
+		 * 3) 스트림을 이용해서 HTML 코드 출력하기
+		 */
+		
+		
+		// 1. 응답하는 문서(데이터)의 형식과 문자 인코딩 지정
+		resp.setContentType("text/html; charset=utf-8");
+		
+		// 2. 출력용 스트림 얻어오기
+		PrintWriter out = resp.getWriter();
+		
+		// 3. 스트림을 이용해서 HTMl 코드 출력하기
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<!DOCTYPE html>");
+		sb.append("<html>");
+		
+		sb.append("<head>");
+		sb.append("<title> /ex1 응답 페이지 </title>");
+		sb.append("</head>");
+		
+		sb.append("<body>");
+		sb.append(String.format("<h1> %s 님의 나이는 %s세입니다.</h1>",inputName,inputAge));
+		sb.append("</body>");
+		
+		sb.append("/html");
+		
+		// sb에 누적된 html코드를 출력시키기 
+		
+		out.print(sb.toString());
+	}
+	
+	                                    
+	
+	
+
+}
